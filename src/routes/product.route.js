4f0e4cb6052a969512042471f5596d3b1c146c32
Chandan <indspunk@gmail.com>
2023-11-05 19:50:26 +0530

    Done

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -27,7 +27,7 @@ app.get("/:slug", async (req, res) => {
 
 app.put("/:id", async (req, res) => {
     try{
-        const updatedProduct = await Product.findByIdAndDelete(req.params.id, req.body, { new: true });
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(200).send({ success: true, product:updatedProduct });
     } catch (error) {
         res.status(500).send({ success: false, message: "Something went wrong", error });
